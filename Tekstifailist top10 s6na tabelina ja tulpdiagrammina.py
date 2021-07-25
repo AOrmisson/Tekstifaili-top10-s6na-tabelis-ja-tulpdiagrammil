18f2d90ca666a11c2f1dc5d23b965e6331dc2ae6
AOrmisson <87547788+AOrmisson@users.noreply.github.com>
2021-07-25 09:43:02 +0300

    Add files via upload

---
+++ Tekstifailist top10 s6na tabelina ja tulpdiagrammina.py
@@ -0,0 +1,53 @@
+import string
+
+filename = input("Enter a file name: ")
+
+with open(filename, "r") as file:
+    text = file.read()
+    words = text.split()
+    table = str.maketrans("", "", string.punctuation)
+    stripped = [char.translate(table) for char in words]
+    #print(stripped)
+
+word_dict = dict()
+
+for word in stripped:
+    if word in word_dict:
+        word_dict[word] = word_dict[word] + 1
+    else:
+        word_dict[word] = 1
+
+for word in tuple(word_dict.keys()):
+    if word.isdigit():
+        del word_dict[word]
+
+#print(sorted(zip(word_dict.values(), word_dict.keys())))
+
+from collections import Counter
+
+C = Counter(word_dict)
+high = C.most_common(10)
+
+print("Top10 sõna")
+print("Nimi: Kogus")
+
+for i in high:
+    print(i[0]," :",i[1]," ")
+
+#
+import pandas as pd
+import matplotlib.pyplot as plot
+
+Top10 = dict(C.most_common(10))
+x = list(Top10.keys())
+y = list(Top10.values())
+
+plot.bar(x, y)
+plot.xticks(rotation=90)
+plot.xlabel("Sõnad")
+plot.ylabel("Kogus")
+plot.title("Top10 enimesinevat sõna")
+plot.show()
+
+
+
